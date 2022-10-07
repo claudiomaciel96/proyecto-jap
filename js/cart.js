1976a398b5383b1d576196583ee3115b6b4189f6
claudiomaciel96 <claumaciel96@gmail.com>
2022-10-06 22:37:30 -0300

    entrega 5 mod 2

--- js/cart.js
+++ js/cart.js
@@ -12,10 +12,29 @@ let articulosCarrito = getJSONData(CART_INFO_URL+idUsuario+EXT_TYPE).then(functi
         <td>${articuloCarrito.name}</td>
         <td>${articuloCarrito.currency} ${articuloCarrito.unitCost}</td>
         <td><div class="col-2">
-          <input  type="number" name="productCostInput"  class="form-control"id="productCostInput" placeholder value="${articuloCarrito.count}" min="0">
+          <input  id ="input-cantidad" type="number" name="input-cantidad"  class="form-control"id="productCostInput" placeholder value="${articuloCarrito.count}" min="1">
       </div></td>
-        <td>COSTO * CANTIDAD</td>
+        <td id="subtotal">${articuloCarrito.currency} ${articuloCarrito.unitCost}</td>
       </tr>`;
+    
       document.getElementById("articulos-acomprar").innerHTML = agregar;
+      
+      document.getElementById("input-cantidad").addEventListener('click', function(){
+        let cantidad= document.getElementById("input-cantidad");
+        document.getElementById("subtotal").innerHTML=`${articuloCarrito.currency} ${articuloCarrito.unitCost * cantidad.value}`;
+      });
+      
     }
+    
+
+    /* document.getElementById("input-cantidad").addEventListener('click', function(){
+      let cantidad= document.getElementById("input-cantidad");
+      document.getElementById("subtotal").innerHTML=`${articulosCarrito[0].currency} ${articulosCarrito[0].unitCost * cantidad.value}`;
+    }); */
 });
+
+function calcularSubtotal(a){
+  
+  document.getElementById("subtotal").innerHTML=`${articulosCarrito[0].currency} ${a}`;
+}
+
