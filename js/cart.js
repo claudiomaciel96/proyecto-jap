aea3b6cc34c0726008c7bd6724e1b05d130c4ecb
claudiomaciel96 <claumaciel96@gmail.com>
2022-10-14 23:01:44 -0300

    entrega 5 mod definitiva

--- js/cart.js
+++ js/cart.js
@@ -1,40 +1,32 @@
 let idUsuario = "25801";
-let articulosCarrito = getJSONData(CART_INFO_URL+idUsuario+EXT_TYPE).then(function(resultObj){
-    if (resultObj.status === "ok"){
-        articulosCarrito = resultObj.data.articles;   
-   
-    }
+let agregar = "";
+let articulosCarrito = getJSONData(CART_INFO_URL + idUsuario + EXT_TYPE).then(function (resultObj) {
+  if (resultObj.status === "ok") {
+    articulosCarrito = resultObj.data.articles;
 
-    for (let articuloCarrito of articulosCarrito){
-        let agregar="";
-        agregar +=`<tr>
-        <th ><img src="${articuloCarrito.image}" width="120" alt="imagen" ></th>
-        <td>${articuloCarrito.name}</td>
-        <td>${articuloCarrito.currency} ${articuloCarrito.unitCost}</td>
-        <td><div class="col-2">
-          <input  id ="input-cantidad" type="number" name="input-cantidad"  class="form-control"id="productCostInput" placeholder value="${articuloCarrito.count}" min="1">
+  }
+
+  for (let i = 0; i < articulosCarrito.length; i++) {
+
+    agregar += `<tr>
+        <th ><img src="${articulosCarrito[i].image}" width="120" alt="imagen" ></th>
+        <td>${articulosCarrito[i].name}</td>
+        <td>${articulosCarrito[i].currency} ${articulosCarrito[i].unitCost}</td>
+        <td><div class="col-7">
+          <input  id ="input-cantidad${i}" type="number" name="input-cantidad"  class="form-control" id="productCostInput" placeholder value="${articulosCarrito[i].count}" min="1">
       </div></td>
-        <td id="subtotal">${articuloCarrito.currency} ${articuloCarrito.unitCost}</td>
+        <td id="subtotal">${articulosCarrito[i].currency} ${articulosCarrito[i].unitCost}</td>
       </tr>`;
-    
-      document.getElementById("articulos-acomprar").innerHTML = agregar;
-      
-      document.getElementById("input-cantidad").addEventListener('click', function(){
-        let cantidad= document.getElementById("input-cantidad");
-        document.getElementById("subtotal").innerHTML=`${articuloCarrito.currency} ${articuloCarrito.unitCost * cantidad.value}`;
-      });
-      
-    }
-    
+    document.getElementById("articulos-acomprar").innerHTML = agregar;
+    document.getElementById("input-cantidad" + i).addEventListener('input', function () {
+      let cantidad = document.getElementById("input-cantidad" + i);
+      document.getElementById("subtotal").innerHTML = `${articulosCarrito[i].currency} ${articulosCarrito[i].unitCost * cantidad.value}`;
+    });
 
-    /* document.getElementById("input-cantidad").addEventListener('click', function(){
-      let cantidad= document.getElementById("input-cantidad");
-      document.getElementById("subtotal").innerHTML=`${articulosCarrito[0].currency} ${articulosCarrito[0].unitCost * cantidad.value}`;
-    }); */
+
+  }
+
 });
 
-function calcularSubtotal(a){
-  
-  document.getElementById("subtotal").innerHTML=`${articulosCarrito[0].currency} ${a}`;
-}
+
 
